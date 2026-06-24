689e271304552b49ff7eb63799e6a19b40c02ce3
Keshav Singhania <worksinghania@gmail.com>
2026-06-24 22:32:39 +0530

    Time: 6 ms (81.79%), Space: 44.4 MB (47.89%) - LeetHub

--- 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.java
+++ 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.java
@@ -1,28 +1,52 @@
 class Solution {
-
     public String reverseWords(String s) {
-        StringBuilder temp = new StringBuilder();
-        StringBuilder ans = new StringBuilder();
-        for(int i = s.length() - 1; i >= 0; i--){
-            if(s.charAt(i) != ' '){
-                temp.append(s.charAt(i));
-            }else{
-                if(temp.length() > 0){
-                     if (ans.length() > 0) {
-                        ans.append(' ');
-                    }
-                    temp.reverse();
-                    ans.append(temp);
-                    temp.setLength(0);
-                }
+        //first trim string on basic of space and store as String array
+        //trim == removing extra spaces from start and end
+        //split == divide strig based on "\\s+" that is one or more extra spaces
+        String [] strArr = s.trim().split(" +");
+
+        StringBuilder sb = new StringBuilder();
+        
+        //will pick string from the back index of string array and append to sb
+        for(int i= strArr.length-1; i>=0; i--){
+            sb.append(strArr[i]);
+            
+            //after every word , we must have add one space 
+            //NOTE : - donot add space after last word
+            if(i>0){
+                sb.append(" ");
             }
         }
-         if (temp.length() > 0) {
-            if (ans.length() > 0) {
-                ans.append(' ');
-            }
-            ans.append(temp.reverse());
-        }
-        return ans.toString();
+        return sb.toString();
     }
 }
+
+
+// class Solution {
+
+//     public String reverseWords(String s) {
+//         StringBuilder temp = new StringBuilder();
+//         StringBuilder ans = new StringBuilder();
+//         for(int i = s.length() - 1; i >= 0; i--){
+//             if(s.charAt(i) != ' '){
+//                 temp.append(s.charAt(i));
+//             }else{
+//                 if(temp.length() > 0){
+//                      if (ans.length() > 0) {
+//                         ans.append(' ');
+//                     }
+//                     temp.reverse();
+//                     ans.append(temp);
+//                     temp.setLength(0);
+//                 }
+//             }
+//         }
+//          if (temp.length() > 0) {
+//             if (ans.length() > 0) {
+//                 ans.append(' ');
+//             }
+//             ans.append(temp.reverse());
+//         }
+//         return ans.toString();
+//     }
+// }
