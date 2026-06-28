ea94fb62530366f9c222e0118a3bc15f150d868a
Keshav Singhania <worksinghania@gmail.com>
2026-06-28 09:52:26 +0530

    Time: 9 ms (67.5%), Space: 77.2 MB (96.25%) - LeetHub

--- 1846-maximum-element-after-decreasing-and-rearranging/1846-maximum-element-after-decreasing-and-rearranging.java
+++ 1846-maximum-element-after-decreasing-and-rearranging/1846-maximum-element-after-decreasing-and-rearranging.java
@@ -1,15 +1,28 @@
+// class Solution {
+//     public int maximumElementAfterDecrementingAndRearranging(int[] arr) {
+//         //first sort it 
+//         Arrays.sort(arr);
+//         int uniqueCount = 1;
+
+//         for(int i = 1; i < arr.length; i++){
+//             if(arr[i] != arr[i-1]){
+//                 uniqueCount++;
+//                 arr[i] = uniqueCount;
+//             }
+//         }
+//         return uniqueCount;
+//     }
+// }
+
 class Solution {
     public int maximumElementAfterDecrementingAndRearranging(int[] arr) {
-        //first sort it 
         Arrays.sort(arr);
-        int uniqueCount = 1;
         arr[0] = 1;
-        for(int i = 1; i < arr.length; i++){
-            if(arr[i] != arr[i-1]){
-                uniqueCount++;
-                arr[i] = uniqueCount;
-            }
+        
+        for (int i = 1; i < arr.length; i++) {
+            arr[i] = Math.min(arr[i], arr[i-1] + 1);
         }
-        return uniqueCount;
+        
+        return arr[arr.length - 1];
     }
 }
