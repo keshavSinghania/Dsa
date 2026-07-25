acfbd8d942b667723b9d481f4c01eafe1bfa5529
Keshav Singhania <worksinghania@gmail.com>
2026-07-25 22:39:13 +0530

    Time: 2 ms (96.07%), Space: 48.9 MB (15.03%) - LeetHub

--- 0338-counting-bits/0338-counting-bits.java
+++ 0338-counting-bits/0338-counting-bits.java
@@ -1,19 +1,30 @@
 class Solution {
     public int[] countBits(int n) {
-        int[] ans = new int[n + 1];
-        for(int i = 0; i < ans.length; i ++){
-            ans[i] = countBit(i);
+        int[] bits = new int[n + 1];
+
+        for (int i = 1; i <= n; i++) {
+            bits[i] = bits[i >> 1] + (i & 1);
+            // bits[i] = bits[i / 2] + (i & 1);
         }
-        return ans;
-    }
-    //function to calculate the number of bit in any number
-    private int countBit(int n){
-        int count = 0;
-        while(n != 0){
-            //check the bit of 0th index if its i increase count then right shift , else only right shift
-            if((n & 1) == 1) count++;
-            n = n >> 1;
-        }
-        return count;
+        return bits;
     }
 }
+// class Solution {
+//     public int[] countBits(int n) {
+//         int[] ans = new int[n + 1];
+//         for(int i = 0; i < ans.length; i ++){
+//             ans[i] = countBit(i);
+//         }
+//         return ans;
+//     }
+//     //function to calculate the number of bit in any number
+//     private int countBit(int n){
+//         int count = 0;
+//         while(n != 0){
+//             //check the bit of 0th index if its i increase count then right shift , else only right shift
+//             if((n & 1) == 1) count++;
+//             n = n >> 1;
+//         }
+//         return count;
+//     }
+// }
