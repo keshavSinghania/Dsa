391f9004bbdd5831d1f2ddf212935e484a601e66
Keshav Singhania <worksinghania@gmail.com>
2026-07-28 18:20:39 +0530

    Time: 1 ms (86.79%), Space: 44.3 MB (39.48%) - LeetHub

--- 0078-subsets/0078-subsets.java
+++ 0078-subsets/0078-subsets.java
@@ -1,28 +1,23 @@
 class Solution {
     public List<List<Integer>> subsets(int[] nums) {
         List<List<Integer>> ans = new ArrayList<>();
-        List<Integer> temp = new ArrayList<>();
+        int noOfSubsets = 1 << nums.length;
 
-        int idx = 0;
-
-        generate(ans, temp, idx, nums);
-        return ans;
-    }
-
-    //function to generate all the subsets using recurison
-    private void generate(List<List<Integer>> ans, List<Integer> temp, int idx, int[] nums){
-        if(nums.length == idx){
-            //add inside the ans 
-            ans.add(new ArrayList<>(temp));
-            return;
+        // go from 0 to noOfSubsets - 1, and for 0 take and for 1 not take
+        for(int i = 0; i < noOfSubsets; i++){
+            //always check for nums.length bits only
+            List<Integer> temp = new ArrayList<>();
+            int mask = i;
+            for(int idx = 0; idx < nums.length; idx++){
+                //start checking bits of i from back if 1 then put that index value or not put
+                if((mask & 1) != 0){
+                    temp.add(nums[idx]);
+                }
+                //right shift i by 1
+                mask = mask >> 1;
+            }
+            ans.add(new ArrayList(temp));
         }
-
-        // take
-        temp.add(nums[idx]);
-        generate(ans, temp, idx + 1, nums);
-
-        //remove current and go for not take
-        temp.remove(temp.size() - 1);
-        generate(ans, temp, idx + 1, nums);
+        return ans;
     }
 }
