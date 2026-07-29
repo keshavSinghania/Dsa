c60859afcf9715d88fec94c4246746ce5c6127eb
Keshav Singhania <worksinghania@gmail.com>
2026-07-29 13:49:13 +0530

    Time: 0 ms (100%), Space: 45.3 MB (50.84%) - LeetHub

--- 0137-single-number-ii/0137-single-number-ii.java
+++ 0137-single-number-ii/0137-single-number-ii.java
@@ -1,22 +1,35 @@
 class Solution {
     public int singleNumber(int[] nums) {
-        int ans = 0;
-        for(int i = 0; i < 32; i++){
-            int setBitCount = 0;
-            for(int num : nums){
-                if((num & (1 << i)) != 0) setBitCount++;
-            }
-            if(setBitCount % 3 != 0){
-                //add one at bit index i
-                ans = ans | (1 << i);
-            }
+        int once = 0;
+        int twos = 0;
+        for(int i = 0; i < nums.length; i++){
+            //add in once of not in two
+            once = (once ^ nums[i]) & (~twos);
+            twos = (twos ^ nums[i]) & (~once);
         }
-        return ans;
+        return once;
     }
 }
 
-//idea 
-// take all the number and write thier bits,
-// now start from back and count number of 1s bit at last position ,
-// if that is multiple of 3 then add then our ans last bit will be 0 else 1
-// do this for all the last bit till 32 
+// class Solution {
+//     public int singleNumber(int[] nums) {
+//         int ans = 0;
+//         for(int i = 0; i < 32; i++){
+//             int setBitCount = 0;
+//             for(int num : nums){
+//                 if((num & (1 << i)) != 0) setBitCount++;
+//             }
+//             if(setBitCount % 3 != 0){
+//                 //add one at bit index i
+//                 ans = ans | (1 << i);
+//             }
+//         }
+//         return ans;
+//     }
+// }
+
+// //idea 
+// // take all the number and write thier bits,
+// // now start from back and count number of 1s bit at last position ,
+// // if that is multiple of 3 then add then our ans last bit will be 0 else 1
+// // do this for all the last bit till 32 
