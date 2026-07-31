6b47dee3b34f9712224d2e78af50090c809ae6da
Keshav Singhania <worksinghania@gmail.com>
2026-07-31 10:58:30 +0530

    Time: 66 ms (8.67%), Space: 48.2 MB (17.34%) - LeetHub

---
+++ 3016-minimum-number-of-pushes-to-type-word-ii/3016-minimum-number-of-pushes-to-type-word-ii.java
@@ -0,0 +1,30 @@
+class Solution {
+    public int minimumPushes(String word) {
+        //creating a hashmap to count frequency of each character
+        HashMap<Character, Integer> map = new HashMap<>();
+
+        //inserting every character from word into hash with its freq
+        for (char ch : word.toCharArray()) {
+            map.put(ch, map.getOrDefault(ch, 0) + 1);
+        }
+
+        //SORT HASHMAP USING LIST BASED ON FREQ
+
+        // converting from hashmap to list
+        List<Map.Entry<Character, Integer>> list = new ArrayList<>(map.entrySet());
+        //sorting descending order
+        list.sort((a, b) -> b.getValue() - a.getValue());
+        int ans = 0;
+        int mult = 1;
+        int keyMapped = 0;
+        for (Map.Entry<Character, Integer> entry : list) {
+            keyMapped++;
+            ans = ans + (entry.getValue() * mult);
+            if(keyMapped == 8){
+                mult++;
+                keyMapped = 0;
+            }
+        }
+        return ans;
+    }
+}
