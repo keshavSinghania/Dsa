f2c16f84135b9a18c15610b02107f637757694bd
Keshav Singhania <worksinghania@gmail.com>
2026-08-03 15:22:24 +0530

    Time: 4 ms (73.88%), Space: 45.4 MB (36.8%) - LeetHub

--- 0496-next-greater-element-i/0496-next-greater-element-i.java
+++ 0496-next-greater-element-i/0496-next-greater-element-i.java
@@ -1,27 +1,31 @@
 class Solution {
     public int[] nextGreaterElement(int[] nums1, int[] nums2) {
+        //to store ans
         int ans[] = new int[nums1.length];
-        //brute force
-        for(int i = 0; i < nums1.length; i++){
-            ans[i] = findNextGreater(nums1[i], nums2);
-        }
-        return ans;
-    }
-    // ..function which will first find the key and then find the next greater element after key
-    private int findNextGreater(int key, int[] nums2){
-        int ans = -1;
-        boolean gotKey = false;
-        for(int i = 0; i < nums2.length && ans == -1; i++){
-            if(!gotKey){
-                if(nums2[i] == key){
-                    gotKey = true;
-                }
+
+        //to store the next greater element of every element of nums1
+        HashMap<Integer, Integer> map = new HashMap<>();
+        Stack<Integer> st = new Stack<>();
+
+        for(int num2 : nums2){
+            //if stack is empty
+            if(st.isEmpty()){
+                st.push(num2);
+            }else if(st.peek() > num2){
+                st.push(num2);
             }else{
-                if(nums2[i] > key){
-                    ans = nums2[i];
-                    break;
+                //pop untill peek is not empty or smaller
+                while(!st.isEmpty() && st.peek() < num2){
+                    //pop and mkae pair
+                    map.put(st.pop(), num2);
                 }
+                //put current elemetn in stack as well
+                st.push(num2);
             }
+        }
+        //our map is ready
+        for(int i = 0 ; i < nums1.length; i++){
+            ans[i] = map.getOrDefault(nums1[i], -1);
         }
         return ans;
     }
