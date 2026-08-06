bef677f6c37ec480ed9089a0157527834cc1de4d
Keshav Singhania <worksinghania@gmail.com>
2026-08-06 10:32:43 +0530

    Time: 1 ms (100%), Space: 42.7 MB (30.89%) - LeetHub

--- 3345-smallest-divisible-digit-product-i/3345-smallest-divisible-digit-product-i.java
+++ 3345-smallest-divisible-digit-product-i/3345-smallest-divisible-digit-product-i.java
@@ -2,16 +2,17 @@ class Solution {
     public int smallestNumber(int n, int t) {
         // we are sure product digits any number btw n to n + 10 ,will be divisible with all the number btw 1 to 10
 
-        for (int i = 0; i < 10; i++) {
-            int current = n + i;
-            int num = current;
+        for(int i = 0; i < 10; i++){
+            int num = n + i;
             int product = 1;
-            while (num > 0) {
+            while(num > 0){
                 product = product * (num % 10);
+                if(product == 0){
+                    break;
+                }
                 num = num / 10;
             }
-            if (product % t == 0)
-                return current;
+            if(product % t == 0) return n + i;
         }
         return -1;
     }
