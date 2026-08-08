e2261dc41921aee9bddc243e19a647a71babd405
Keshav Singhania <worksinghania@gmail.com>
2026-08-08 16:02:20 +0530

    Time: 6 ms (21.12%), Space: 48.6 MB (5.35%) - LeetHub

--- 0735-asteroid-collision/0735-asteroid-collision.java
+++ 0735-asteroid-collision/0735-asteroid-collision.java
@@ -1,42 +1,51 @@
 class Solution {
     public int[] asteroidCollision(int[] asteroids) {
+        Stack<Integer> st = new Stack<>();
 
+        //covert into list
         ArrayList<Integer> list = new ArrayList<>();
-
-        // Convert array to list
+        //Convert array to list
         for (int num : asteroids) {
             list.add(num);
         }
+        for(int i = list.size() - 1; i >= 0; i--){
+            int current = list.get(i);
 
-        while (true) {
-            boolean isCollid = false;
-            for (int i = 0; i < list.size() - 1; i++) {
-
-                int curr = list.get(i);
-                int next = list.get(i + 1);
-
-                if (curr > 0 && next < 0) {
-                    // Collision
-                    if (curr == -next) {
-                        // Both explode
-                        list.remove(i + 1);
-                        list.remove(i);
-                    } else if (curr > -next) {
-                        // next explodes
-                        list.remove(i + 1);
-                    } else {
-                        // curr explodes
-                        list.remove(i);
-                    }
-                    isCollid = true;
-                    break;
+            if(current < 0){
+                st.push(current);
+                list.remove(i);
+                continue;
+            }
+            
+            if(!st.isEmpty() && current > 0){
+                while(!st.isEmpty() && current > -st.peek()){
+                    st.pop();
+                }
+                if(!st.isEmpty() && current == -st.peek()){
+                    st.pop();
+                    list.remove(i);
+                }else if(!st.isEmpty()){
+                    //if stack has element (-ve won) hence destroy this +ve
+                    list.remove(i);
+                }else if(st.isEmpty()){
+                    //if stack is empty +ve won therefore continue
+                    continue;
                 }
             }
-            if (!isCollid)
-                break;
         }
 
-        // Convert list to array manually
+         // in their original left-to-right order
+        ArrayList<Integer> negative = new ArrayList<>();
+
+        while (!st.isEmpty()) {
+            negative.add(st.pop());
+        }
+
+        // Put surviving negatives before surviving positives
+        negative.addAll(list);
+        list = negative;
+
+        //we have final ans list
         int[] ans = new int[list.size()];
         for (int i = 0; i < list.size(); i++) {
             ans[i] = list.get(i);
@@ -44,3 +53,52 @@ class Solution {
         return ans;
     }
 }
+
+
+
+// class Solution {
+//     public int[] asteroidCollision(int[] asteroids) {
+
+//         ArrayList<Integer> list = new ArrayList<>();
+
+//         // Convert array to list
+//         for (int num : asteroids) {
+//             list.add(num);
+//         }
+
+//         while (true) {
+//             boolean isCollid = false;
+//             for (int i = 0; i < list.size() - 1; i++) {
+
+//                 int curr = list.get(i);
+//                 int next = list.get(i + 1);
+
+//                 if (curr > 0 && next < 0) {
+//                     // Collision
+//                     if (curr == -next) {
+//                         // Both explode
+//                         list.remove(i + 1);
+//                         list.remove(i);
+//                     } else if (curr > -next) {
+//                         // next explodes
+//                         list.remove(i + 1);
+//                     } else {
+//                         // curr explodes
+//                         list.remove(i);
+//                     }
+//                     isCollid = true;
+//                     break;
+//                 }
+//             }
+//             if (!isCollid)
+//                 break;
+//         }
+
+//         // Convert list to array manually
+//         int[] ans = new int[list.size()];
+//         for (int i = 0; i < list.size(); i++) {
+//             ans[i] = list.get(i);
+//         }
+//         return ans;
+//     }
+// }
