6f072c23051e583b41807dd4e0a511249d0bff36
Keshav Singhania <worksinghania@gmail.com>
2026-08-10 18:31:47 +0530

    Time: 22 ms (57.09%), Space: 47.9 MB (6%) - LeetHub

--- 2104-sum-of-subarray-ranges/2104-sum-of-subarray-ranges.java
+++ 2104-sum-of-subarray-ranges/2104-sum-of-subarray-ranges.java
@@ -1,21 +1,86 @@
 class Solution {
     public long subArrayRanges(int[] nums) {
-        long ans = 0;
-        // let me solve this using brute force
-        for(int i = 0; i < nums.length; i++){
-            int smallestElement = nums[i];
-            int largestElement = nums[i];
-            for(int j = i; j < nums.length; j++){
-                if(smallestElement > nums[j]){
-                    smallestElement = nums[j];
-                }else if(largestElement < nums[j]){
-                    largestElement = nums[j];
-                }
+        Stack<Integer> st = new Stack<>();
+        int[] nextSmaller = new int[nums.length];
+        int[] nextGreater = new int[nums.length];
+        int[] prevSmaller = new int[nums.length];
+        int[] prevGreater = new int[nums.length];
 
-                //we have updated smallest and largest element
-                ans += (largestElement - smallestElement);
+        // FUNCTION TO FIND THE NEXT SMALLEST ELEMENT INDEX IN THE ARRAY
+        for (int i = nums.length - 1; i >= 0; i--) {
+            while (!st.isEmpty() && nums[st.peek()] > nums[i]) {
+                st.pop();
             }
+            if (st.isEmpty()) {
+                nextSmaller[i] = -1;
+            } else {
+                nextSmaller[i] = st.peek();
+            }
+            st.add(i);
         }
-        return ans;
+
+        // clearing our stack
+        st.clear();
+
+        // FUNCTION TO FIND THE NEXT GREATER ELEMENT INDEX IN THE ARRAY
+        for (int i = nums.length - 1; i >= 0; i--) {
+            while (!st.isEmpty() && nums[st.peek()] < nums[i]) {
+                st.pop();
+            }
+            if (st.isEmpty()) {
+                nextGreater[i] = -1;
+            } else {
+                nextGreater[i] = st.peek();
+            }
+            st.add(i);
+        }
+
+        // clearing our stack
+        st.clear();
+
+        // FUNCTION TO FIND THE PREV SMALLEST ELEMENT
+        for (int i = 0; i < nums.length; i++) {
+            while (!st.isEmpty() && nums[st.peek()] >= nums[i]) {
+                st.pop();
+            }
+            if (st.isEmpty()) {
+                prevSmaller[i] = -1;
+            } else {
+                prevSmaller[i] = st.peek();
+            }
+            st.add(i);
+        }
+
+        // clearing our stack
+        st.clear();
+
+        // FUNCTION TO FIND THE PREV GREATER ELEMENT
+        for (int i = 0; i < nums.length; i++) {
+            while (!st.isEmpty() && nums[st.peek()] <= nums[i]) {
+                st.pop();
+            }
+            if (st.isEmpty()) {
+                prevGreater[i] = -1;
+            } else {
+                prevGreater[i] = st.peek();
+            }
+            st.add(i);
+        }
+
+        // clearing our stack
+        st.clear();
+        
+        long minSum = 0;
+        long maxSum = 0;
+        for(int i = 0; i < nums.length; i++){
+            minSum += (long) (i - prevSmaller[i]) *
+                      (nextSmaller[i] == -1 ? nums.length - i : nextSmaller[i] - i) *
+                      nums[i];
+
+            maxSum += (long) (i - prevGreater[i]) *
+                      (nextGreater[i] == -1 ? nums.length - i : nextGreater[i] - i) *
+                      nums[i];
+        }
+        return maxSum - minSum;
     }
 }
