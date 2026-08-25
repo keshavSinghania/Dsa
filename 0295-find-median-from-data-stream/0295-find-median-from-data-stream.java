5d0880a7f68a4157781ef37e7fa328a9bddfc031
Keshav Singhania <worksinghania@gmail.com>
2026-08-25 15:56:21 +0530

    Time: 121 ms (89.34%), Space: 114.2 MB (5.17%) - LeetHub

---
+++ 0295-find-median-from-data-stream/0295-find-median-from-data-stream.java
@@ -0,0 +1,52 @@
+class MedianFinder {
+    PriorityQueue<Double> leftMaxHeap = new PriorityQueue<>((a, b) -> Double.compare(b, a));
+    PriorityQueue<Double> rightMinHeap = new PriorityQueue<>();
+    public MedianFinder() {
+    }
+    
+    public void addNum(int num) {
+        double num1 = num;
+        if(leftMaxHeap.isEmpty()){
+            leftMaxHeap.offer(num1);
+            return;
+        }
+
+        double leftPeak = leftMaxHeap.peek();
+        if(num1 < leftPeak){
+            leftMaxHeap.offer(num1);
+        }else{
+            rightMinHeap.offer(num1);
+        }
+
+        //make sure size1 + 1 == size2 || size1 == size1 || or size1 = size2 + 1
+
+        if(leftMaxHeap.size() >  rightMinHeap.size() + 1){
+            rightMinHeap.offer(leftMaxHeap.poll());
+            return;
+        }else if(rightMinHeap.size() > leftMaxHeap.size() + 1){
+            leftMaxHeap.offer(rightMinHeap.poll());
+            return;
+        }
+        return;
+    }
+    
+    public double findMedian() {
+
+        //if odd length
+        if(leftMaxHeap.size() > rightMinHeap.size()){
+            return leftMaxHeap.peek();
+        }else if(leftMaxHeap.size() < rightMinHeap.size()){
+            return rightMinHeap.peek();
+        }else{
+            //even length case
+            return (leftMaxHeap.peek() + rightMinHeap.peek()) / 2.0;
+        }
+    }
+}
+
+/**
+ * Your MedianFinder object will be instantiated and called as such:
+ * MedianFinder obj = new MedianFinder();
+ * obj.addNum(num);
+ * double param_2 = obj.findMedian();
+ */
