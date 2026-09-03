101dacc79e2b9e2a9085242e094eee135c8e9472
Keshav Singhania <worksinghania@gmail.com>
2026-09-03 21:27:10 +0530

    Time: 2 ms (29.54%), Space: 47.5 MB (8.19%) - LeetHub

--- 0045-jump-game-ii/0045-jump-game-ii.java
+++ 0045-jump-game-ii/0045-jump-game-ii.java
@@ -1,20 +1,19 @@
 class Solution {
     public int jump(int[] nums) {
-        int toReach = nums.length - 1;
-        int possibleIdx = -1;
-        int jump = 0;
-        if(nums.length == 1) return 0;
-        if(nums.length == 0) return 0;
-        while(true){
-            for(int j = toReach - 1; j >= 0; j--){
-                int dist = toReach - j;
-                if(dist <= nums[j]){
-                    possibleIdx = j;
-                }
-            }
-            toReach = possibleIdx;
-            jump++;
-            if(toReach == 0) return jump;
+        if(nums.length == 0 || nums.length == 1){
+            return 0;
         }
+        int lastJumpIndex = 0;
+        int totalJump = 0;
+        int currentMax = nums[0];
+
+        for(int i = 0; i < nums.length - 1; i++){
+            currentMax = Math.max(currentMax, i + nums[i]);
+            if(lastJumpIndex == i){
+                lastJumpIndex = currentMax;
+                totalJump++;
+            }
+        }
+        return totalJump;
     }
 }
