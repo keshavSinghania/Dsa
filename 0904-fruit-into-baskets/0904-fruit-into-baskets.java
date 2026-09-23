e2236677c3f0149b21302a13f429330cebe4b524
Keshav Singhania <worksinghania@gmail.com>
2026-09-23 14:32:06 +0530

    Time: 46 ms (88.3%), Space: 66.1 MB (95.71%) - LeetHub

--- 0904-fruit-into-baskets/0904-fruit-into-baskets.java
+++ 0904-fruit-into-baskets/0904-fruit-into-baskets.java
@@ -1,31 +1,26 @@
 class Solution {
     public int totalFruit(int[] fruits) {
-        int typeOfFruits = 0;
-        HashMap<Integer,Integer> map = new HashMap<>();
-        int count = 0;
-        int maxFruitCount = 0;
-        int ptr1 = 0;
-        for(int ptr2 = 0; ptr2 < fruits.length; ptr2++){
-            //if we get new fruit , and we already having 2 different types of fruits in baskets
-            while(typeOfFruits == 2 && !map.containsKey(fruits[ptr2])){
-                //delete last element untill typeOfFruits becomes less than 2
-                map.put(fruits[ptr1], map.get(fruits[ptr1]) - 1);
-                if(map.get(fruits[ptr1]) == 0){
-                    typeOfFruits--;
-                    map.remove(fruits[ptr1]);
+        int maxCount = 0;
+        HashMap<Integer, Integer> map = new HashMap<>();
+        int left = 0;
+        int right = 0;
+
+        for(; right < fruits.length; right++){
+            map.put(fruits[right], map.getOrDefault(fruits[right], 0) + 1);
+
+            while(map.size() > 2){
+                int temp = map.get(fruits[left]);
+                if(temp == 1){
+                    map.remove(fruits[left]);
+                }else{
+                    temp--;
+                    map.put(fruits[left], temp);
                 }
-                ptr1++;
-                count--;
+                left++;
             }
-            if(!map.containsKey(fruits[ptr2])){
-                typeOfFruits++;
-                map.put(fruits[ptr2], 1);
-            }else{
-                map.put(fruits[ptr2], map.get(fruits[ptr2]) + 1);
-            }
-            count++;
-            maxFruitCount = Math.max(count, maxFruitCount);
+            int count = right - left + 1;
+            maxCount = Math.max(maxCount, count);
         }
-        return maxFruitCount;
+        return maxCount;
     }
 }
